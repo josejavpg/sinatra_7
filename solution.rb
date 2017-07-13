edcc175b61274267a00dce040def47836f8fdac2
josejavpg <josejavpg@gmail.com>
2017-07-13 09:29:19 -0500

    Add restriction to txtMesanje

--- solution.rb
+++ solution.rb
@@ -5,13 +5,19 @@ get '/' do
 end
 
 post '/respuesta' do
+	
 	@mensaje= params[:txtMensaje]
-	if @mensaje == @mensaje.upcase
-		@mensaje = "Ahhh si, #{@mensaje.downcase}!"
+	if !@mensaje.empty? 
+		if @mensaje == @mensaje.upcase
+			@mensaje = "Ahhh si, #{@mensaje.downcase}!"
+		else
+			@mensaje = "Habla más duro mijito"
+		end
+		erb :respuesta
 	else
-		@mensaje = "Habla más duro mijito"
+		erb :index
 	end
-	erb :respuesta
+	
 end
 # post '/' do
 #   "Hello World"
